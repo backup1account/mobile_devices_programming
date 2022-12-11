1d79f89bbbfc752471e66f92dda7a7f082d8f377
backup1account <103818036+backup1account@users.noreply.github.com>
2022-12-12 00:59:26 +0100

    StudentsHardLife app updated 2

--- List3/app/src/main/java/com/example/list3/dialogs/CreateItemDialogFragment.kt
+++ List3/app/src/main/java/com/example/list3/dialogs/CreateItemDialogFragment.kt
@@ -44,37 +44,3 @@ class CreateItemDialogFragment(
         const val TAG = "AddItemDialog"
     }
 }
-
-
-
-//class EditItemDialogFragment(
-//    private val onDialogClickHandler: OnButtonClickListener,
-//    private val itemId: Int
-//) : DialogFragment() {
-//
-//    @SuppressLint("InflateParams")
-//    override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
-//        return activity?.let {
-//            val builder = AlertDialog.Builder(it)
-//            val inflater = requireActivity().layoutInflater
-//            val view = inflater.inflate(R.layout.edit_item_dialog, null)
-//
-//            val newTitle: EditText by lazy { view.findViewById(R.id.edit_exercise_title) }
-//            val newDescription: EditText by lazy { view.findViewById(R.id.edit_exercise_description) }
-//
-//            builder.setView(view)
-//                .setPositiveButton("Confirm") { _, _ ->
-//                    onDialogClickHandler.onSubmitDialogClickListener(
-//                        newTitle.text.toString(),
-//                        newDescription.text.toString(),
-//                        itemId
-//                    )
-//                }
-//                .setNegativeButton("Cancel") { _, _ -> dismiss() }
-//            builder.create()
-//        } ?: throw IllegalStateException("Activity cannot be null")
-//    }
-//
-//    companion object {
-//        const val TAG = "EditItemDialog"
-//    }
